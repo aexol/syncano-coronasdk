7ea2c6353c7fa54180154e8bc73c7d57912689b7
aexol <aexol@aexol.com>
2015-09-08 13:09:05 +0200

    pierwszy commit

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+API_KEY = "90ca3b77844ecc8a592535679c7e70194808aad7"
+INSTANCE_NAME = "rough-snow-3508"
+
+Syncano = require "syncano"
+syn = Syncano(API_KEY,INSTANCE_NAME)
+syn:getDataObject("recipe",6)
---
+++ syncano.lua
@@ -0,0 +1,42 @@
+local Syncano = {}
+Syncano.prototype = { apiKey = ""}
+Syncano.__index = Syncano
+setmetatable(Syncano,{
+	__call = function(cls,...)
+		return cls.new(...)
+	end,
+	})
+function Syncano.new(apiKey,instance_name)
+	local self = setmetatable({},Syncano)
+	self.apiKey = apiKey
+	self.instance_name = instance_name
+	return self
+end
+function Syncano:getDataObject(class,id)
+	local function networkListener( event )
+	    if ( event.isError ) then
+	    else
+	        print ( "RESPONSE: " .. event.response )
+	    end
+	end
+	local headers = {}
+	headers["X-API-KEY"] = self.apiKey
+	headers["Content-Type"]="application/json"
+	local params = {}
+	params.headers = headers
+	network.request( "https://api.syncano.io/v1/instances/"..self.instance_name.."/classes/"..class.."/objects/"..id.."/", "GET", networkListener, params )
+end
+function Syncano:test()
+	local function networkListener( event )
+	    if ( event.isError ) then
+	    else
+	        print ( "RESPONSE: " .. event.response )
+	    end
+	end
+	local headers = {}
+	headers["X-API-KEY"] = self.apiKey
+	local params = {}
+	params.headers = headers
+	network.request( "https://api.syncano.io/v1/instances/", "GET", networkListener, params )
+end
+return Syncano
